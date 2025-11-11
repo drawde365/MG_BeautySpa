07b4b1ef136b9a9f11ab878b508ec7c4c3f43275
AlvaroArmasJ <alvaro.armasj@pucp.edu.pe>
2025-11-11 09:11:12 -0500

    Create Usuario.java

---
+++ backend JAVA/SoftInvWS/src/main/java/pe/edu/pucp/softinv/services/softinvws/Usuario.java
@@ -0,0 +1,23 @@
+package pe.edu.pucp.softinv.services.softinvws;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+import pe.edu.pucp.softinv.business.BO.Impl.UsuarioBO;
+import pe.edu.pucp.softinv.model.Personas.UsuarioDTO;
+
+@WebService(serviceName = "Usuario")
+public class Usuario {
+
+    private UsuarioBO usuario;
+
+	public Usuario() {
+        usuario = new UsuarioBO();
+    }
+
+    @WebMethod(operationName = "IniciarSesion")
+    public boolean inicioSesion(@WebParam(name="correoElectronico") String correoElectronico, @WebParam(name="contrasenha") String contrasenha) {
+        return usuario.inicioSesion(correoElectronico, contrasenha);
+    }
+
+}
