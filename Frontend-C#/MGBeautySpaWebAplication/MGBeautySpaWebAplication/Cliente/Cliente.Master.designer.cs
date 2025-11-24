8cceeea971132d2407a8e1fcbf181094ee2137a6
unknown <josetsamuelhy25@gmail.com>
2025-11-23 21:53:30 -0500

    LinksRedesYArregladoFooterCliente

--- Frontend-C#/MGBeautySpaWebAplication/MGBeautySpaWebAplication/Cliente/Cliente.Master.designer.cs
+++ Frontend-C#/MGBeautySpaWebAplication/MGBeautySpaWebAplication/Cliente/Cliente.Master.designer.cs
@@ -15,6 +15,15 @@ namespace MGBeautySpaWebAplication.Cliente
     {
 
         /// <summary>
+        /// TitleContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder TitleContent;
+
+        /// <summary>
         /// HeadContent control.
         /// </summary>
         /// <remarks>
