9a12d3a1cd9224da86a14862081cb6ecae9959b6
Flavio <flavios0226@gmail.com>
2025-11-27 17:57:32 -0500

    filtro

--- Frontend-C#/MGBeautySpaWebAplication/SoftInvBusiness/PedidoBO.cs
+++ Frontend-C#/MGBeautySpaWebAplication/SoftInvBusiness/PedidoBO.cs
@@ -101,9 +101,9 @@ namespace SoftInvBusiness
             return pedidoSOAP.modificarProductoTipo(productoTipo);
         }
 
-        public IList<pedidoDTO> listarTodoPedidosPaginado()
+        public IList<pedidoDTO> listarTodoPedidosPaginado(int pagina)
         {
-            return pedidoSOAP.listarTodoPedidosPaginado();
+            return pedidoSOAP.listarTodoPedidosPaginado(pagina);
         }
 
         public int obtenerPaginas()
